b8ae750d50a5a4cdf1489061ce95cceeabeaedbe
Miller Nguyen <miller_nguyen@hotmail.com>
2025-11-20 17:02:02 +0800

    good to good

--- Data/src/Services/UserService.cs
+++ Data/src/Services/UserService.cs
@@ -1,8 +1,51 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using DevExpress.Data.Filtering;
+using DevExpress.Xpo;
+using NodPT.Data.Models;
+using NodPT.Data.Services;
 
 public class UserService
 {
+    // verify if user is active, approved, and not banned
+    public static bool IsUserValid(string firebaseUId, UnitOfWork session)
+    {
+        try
+        {
+            User? user = session.FindObject<User>(CriteriaOperator.Parse("FirebaseUid=?", firebaseUId));
+            session.AutoCreateOption.ToString();
+            var users = session.Query<User>().ToList();
+            return user != null && user.Active && user.Approved && !user.Banned;
+        }
+        catch (Exception ex)
+        {
+            LogService.LogError(ex, firebaseUId, "IsUserValid");
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// get user by firebaseUId if active, approved, and not banned
+    /// </summary>
+    /// <param name="firebaseUId"></param>
+    /// <param name="session"></param>
+    /// <returns></returns>
+    public static User? GetUser(string firebaseUId, UnitOfWork session)
+    {
+        try
+        {
+            var user = session.FindObject<User>(CriteriaOperator.Parse("FirebaseUid=?", firebaseUId));
+            if (user != null && user.Active && user.Approved && !user.Banned)
+                return user;
+        }
+        catch (Exception ex)
+        {
+            LogService.LogError(ex, firebaseUId, "GetUser");
+        }
+        return null;
+    }
+
     // UserService implementation
     public static string? GetFirebaseUIDFromContent(ClaimsPrincipal user)
     {
